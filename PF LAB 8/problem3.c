acab698baee26332479b9b61ccf1419fdd7bdd08
Abdullah Hakro <hakroabdullah17@gmail.com>
2024-10-28 23:23:06 +0500

    Add files via upload

---
+++ PF LAB 8/problem3.c
@@ -0,0 +1,42 @@
+//Create a 3D array representing 2 pages of a 3x3 matrix. Initialize it and find the sum of all the
+//elements on each page.
+
+#include<stdio.h>
+int main(){
+
+    int arr[2][3][3];
+    int i , j , k;
+
+    for(i = 0; i < 2; i++){
+        for(j = 0; j < 3; j++){
+            for(k = 0; k < 3; k++){
+                printf("Enter the %d in x %d in y and %d in z element :" , i+1 , j+1 , k+1);
+                scanf("%d",&arr[i][j][k]);
+            }
+        }
+    }
+    
+    int sumpagefirst = 0;
+
+    for(i = 0; i < 1; i++){
+        for(j = 0; j < 3; j++){
+            for(k = 0; k < 3; k++){
+                sumpagefirst += arr[i][j][k];
+            }
+        }
+    }
+    
+    int sumpagesecond = 0;
+
+     for(i = 1; i < 2; i++){
+        for(j = 0; j < 3; j++){
+            for(k = 0; k < 3; k++){
+                sumpagesecond += arr[i][j][k];
+            }
+        }
+    }
+    printf("the sum of first layer is : %d \n", sumpagefirst);
+    printf("the sum of second layer is : %d \n", sumpagesecond);
+
+    return 0;
+}
